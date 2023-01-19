cfb6a3e3f68473b0d0698a394fc3ca48a82e862e
logzinga <65839350+logzinga@users.noreply.github.com>
2023-01-19 21:39:31 +1100

    im stupid

--- src/main/java/com/logzinga/antitnt/AntiTNT.java
+++ src/main/java/com/logzinga/antitnt/AntiTNT.java
@@ -5,26 +5,28 @@ import org.bukkit.block.Block;
 import org.bukkit.block.data.type.TNT;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.EventListener;
 
-public final class AntiTNT extends JavaPlugin implements EventListener {
+public final class AntiTNT extends JavaPlugin implements EventListener, Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(this, this);
     }
     @EventHandler
-    public void onPlace(BlockPlaceEvent e) {
+    public void onBlockPlace(BlockPlaceEvent e) {
       Block block = e.getBlock();
-      Player player = e.getPlayer();
       Material getBlockType = block.getType();
+
       if (getBlockType == Material.TNT) {
-          e.setCancelled(true);
+          Player player = e.getPlayer();
+          block.setType(Material.DIRT);
           player.kickPlayer("TNT Placed");
       }
     }
