63d84cffe3d4509caf58fcdd843f25ef7a780c3a
logzinga <65839350+logzinga@users.noreply.github.com>
2023-01-19 20:21:20 +1100

    Initial Commit

---
+++ src/main/java/com/logzinga/antitnt/AntiTNT.java
@@ -0,0 +1,17 @@
+package com.logzinga.antitnt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AntiTNT extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
